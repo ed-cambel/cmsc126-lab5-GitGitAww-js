397d867ad782b1938d2809ef53d1887072715b1f
“EDRIELLEN <egcambel@up.edu.ph>
2026-03-18 20:55:40 +0800

    feat: link external JS functions in HTML

--- lab5_scripts.js
+++ lab5_scripts.js
@@ -1,1 +1,37 @@
+/*
+    Cambel - HTML Structure + CSS Styling
+    Espartero - Backend Functions
+    Fallarme - Backend Functions
+    Gelvezon - CSS Styling
+*/
+
+function time_now() {
+    // code here
+    document.getElementById("dateOutput").innerHTML = "For checking";
+}
+
+
+function add_student() {
+    // code here
+}
+
+function find_student() {
+    // code here
+}
+
+function display_student_list() {
+    // code here
+}
+
+function validate_form() {
+    // code here
+}
+
+function generate_student_id() {
+    // code here
+}
+
+function validate_student_id() {
+    // code here
+}
 
